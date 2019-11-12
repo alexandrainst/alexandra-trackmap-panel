6f98c19c3c2e27fc43bce3c63fff48b784354472
Anders Ringsborg Aarenstrup Hansen <anders.hansen@alexandra.dk>
2019-11-12 15:17:39 +0100

    Change leaflet import to local. Updated libraries to take a leaflet instance, and added license.

--- src/map_ctrl.js
+++ src/map_ctrl.js
@@ -1,11 +1,14 @@
 import { MetricsPanelCtrl } from 'grafana/app/plugins/sdk';
 import {defaults} from 'lodash';
-import 'leaflet';
+import * as leaflet from 'leaflet';
 import 'leaflet-ant-path';
-import './lib/hexbinLayer.js';
-import './lib/leaflet-heat.js';
+import { hexbinInit } from './lib/hexbinLayer.js';
+import { heatmapInit } from './lib/leaflet-heat.js';
 import './css/hexbin.css';
 import './css/map-panel.css';
+
+hexbinInit(leaflet);
+heatmapInit(leaflet);
 
 const panelDefaults = {
   mapOptions: {
@@ -105,15 +108,15 @@ export class MapCtrl extends MetricsPanelCtrl {
     }
     if (this.points.length > 0) {
       if (this.panel.mode === this.modeTypes[0]) {
-        this.mapLayer = MapCtrl.L.hexbinLayer(this.panel.hexbin).hoverHandler(MapCtrl.L.HexbinHoverHandler.tooltip());
+        this.mapLayer = leaflet.hexbinLayer(this.panel.hexbin).hoverHandler(leaflet.HexbinHoverHandler.tooltip());
         this.mapLayer.data(this.points);
       } else if (this.panel.mode === this.modeTypes[1]) {
-        this.mapLayer = MapCtrl.L.heatLayer(this.points);
+        this.mapLayer = leaflet.heatLayer(this.points);
       } else if (this.panel.mode === this.modeTypes[2]) {
-        this.mapLayer = MapCtrl.L["polyline"].antPath(this.points, this.panel.antpath);
+        this.mapLayer = leaflet["polyline"].antPath(this.points, this.panel.antpath);
       } else {
         console.log("invalid mode type, defaulting to hexbin");
-        this.mapLayer = MapCtrl.L.hexbinLayer(this.panel.hexbin).hoverHandler(MapCtrl.L.HexbinHoverHandler.tooltip());
+        this.mapLayer = leaflet.hexbinLayer(this.panel.hexbin).hoverHandler(leaflet.HexbinHoverHandler.tooltip());
         this.mapLayer.data(this.points);
       }
       this.mapLayer.addTo(this.map);
@@ -136,8 +139,8 @@ export class MapCtrl extends MetricsPanelCtrl {
   }
 
   onPanelInitialized() {
-    this.map = L["map"]('map_' + this.panel.id).setView(this.panel.mapOptions.latlon, this.panel.mapOptions.zoom);
-    L["tileLayer"](this.panel.mapOptions.url, {
+    this.map = leaflet["map"]('map_' + this.panel.id).setView(this.panel.mapOptions.latlon, this.panel.mapOptions.zoom);
+    leaflet["tileLayer"](this.panel.mapOptions.url, {
       attribution: this.panel.mapOptions.attribution,
       maxZoom: 19
     }).addTo(this.map);
@@ -258,4 +261,3 @@ export class MapCtrl extends MetricsPanelCtrl {
 }
 
 MapCtrl.templateUrl = "partials/module.html";
-MapCtrl.L = L;
