8fd2758ec4c2562c2dea27b2f9f530cca141cbb4
Jack Ord <36296721+j-or@users.noreply.github.com>
2020-10-20 16:39:05 +0200

    #18 add grafana 7 compatibility (#24)
    * Add Grafana 7 compatibility
    
    * Update license in package.json
    
    * Update README.md
    
    Co-authored-by: Alexandre Alapetite <alexandre.alapetite@alexandra.dk>
    
    * Add dist, update gitignore
    
    * Fix map not updating on panel resize
    
    Co-authored-by: Alexandre Alapetite <alexandre.alapetite@alexandra.dk>

---
+++ src/TrackMapPanel.tsx
@@ -0,0 +1,186 @@
+import React, { ReactElement, useEffect } from 'react';
+import { PanelProps } from '@grafana/data';
+import { getLocationSrv } from '@grafana/runtime';
+import { TrackMapOptions, Position } from 'types';
+import { css, cx } from 'emotion';
+import { stylesFactory } from '@grafana/ui';
+import { FeatureCollection, Feature } from 'geojson';
+import { Map, TileLayer, Marker, Popup, withLeaflet } from 'react-leaflet';
+import { Icon, LeafletEvent, LatLngBounds } from 'leaflet';
+import './leaflet.css';
+import 'leaflet/dist/leaflet.css';
+import { useRef } from 'react';
+
+const AntPath = require('react-leaflet-ant-path').default;
+const HeatmapLayer = require('react-leaflet-heatmap-layer').default;
+const HexbinLayer = require('react-leaflet-d3').HexbinLayer;
+
+interface Props extends PanelProps<TrackMapOptions> {}
+
+export const TrackMapPanel: React.FC<Props> = ({ options, data, width, height }) => {
+  const styles = getStyles();
+  const mapRef = useRef<Map | null>(null);
+
+  const WrappedHexbinLayer: any = withLeaflet(HexbinLayer);
+
+  const mark = new Icon({
+    iconUrl: require('img/marker.png'),
+    iconSize: [options.marker.size, options.marker.size],
+  });
+
+  useEffect(() => {
+    if (mapRef.current !== null) {
+      const bounds = mapRef.current.leafletElement.getBounds();
+      updateMap(bounds);
+    }
+    // eslint-disable-next-line
+  }, []);
+
+  const latitudes: number[] | undefined = data.series[0].fields
+    .find(f => f.name === 'latitude' || f.name === 'lat')
+    ?.values.toArray();
+  const longitudes: number[] | undefined = data.series[0].fields
+    .find(f => f.name === 'longitude' || f.name === 'lon')
+    ?.values.toArray();
+  const intensities: string[] | undefined = data.series[0].fields.find(f => f.name === 'intensity')?.values.toArray();
+
+  const positions: Position[] | undefined = latitudes?.map((latitude, index) => {
+    return {
+      latitude,
+      longitude: longitudes !== undefined ? longitudes[index] : 0,
+    };
+  });
+
+  const heatData: any[] = [];
+  const antData: number[][] = [];
+  const hexData: FeatureCollection = {
+    type: 'FeatureCollection',
+    features: [],
+  };
+
+  positions?.forEach((p, i) => {
+    heatData.push([p.latitude, p.longitude, intensities !== undefined ? intensities[i] : '']);
+    antData.push([p.latitude, p.longitude]);
+    hexData.features.push({
+      type: 'Feature',
+      id: i,
+      geometry: {
+        type: 'Point',
+        coordinates: [p.longitude, p.latitude],
+      },
+    } as Feature);
+  });
+
+  const markers: ReactElement[] = [];
+  positions?.forEach((p, i) => {
+    markers.push(
+      <Marker key={i} position={[p.latitude, p.longitude]} icon={mark}>
+        <Popup>
+          {p.latitude}, {p.longitude}
+        </Popup>
+      </Marker>
+    );
+  });
+
+  const antOptions = {
+    delay: options.ant.delay,
+    dashArray: [10, 20],
+    weight: options.ant.weight,
+    color: options.ant.color,
+    pulseColor: options.ant.pulseColor,
+    paused: options.ant.paused,
+    reverse: options.ant.reverse,
+  };
+
+  const hexbinOptions = {
+    opacity: options.hex.opacity,
+    colorScaleExtent: [1, undefined],
+    radiusScaleExtent: [1, undefined],
+    colorRange: [options.hex.colorRangeFrom, options.hex.colorRangeTo],
+    radiusRange: [options.hex.radiusRangeFrom, options.hex.radiusRangeTo],
+  };
+
+  const onMapLoad = (event: LeafletEvent) => {
+    updateMap(event.target.getBounds());
+  };
+
+  const onMapMoveEnd = (event: LeafletEvent) => {
+    if (mapRef.current !== null) {
+      mapRef.current.leafletElement.invalidateSize();
+    }
+    updateMap(event.target.getBounds());
+  };
+
+  const updateQueryVariables = (minLat: number, minLon: number, maxLat: number, maxLon: number) => {
+    getLocationSrv().update({
+      query: {
+        'var-minLat': minLat,
+        'var-maxLat': maxLat,
+        'var-minLon': minLon,
+        'var-maxLon': maxLon,
+      },
+      partial: true,
+      replace: true,
+    });
+  };
+
+  const updateMap = (bounds: LatLngBounds) => {
+    const minLat = bounds.getSouthWest().lat;
+    const minLon = bounds.getSouthWest().lng;
+    const maxLat = bounds.getNorthEast().lat;
+    const maxLon = bounds.getNorthEast().lng;
+    if (options.map.useBoundsInQuery) {
+      updateQueryVariables(minLat, minLon, maxLat, maxLon);
+    }
+  };
+
+  return (
+    <div
+      className={cx(
+        styles.wrapper,
+        css`
+          width: ${width}px;
+          height: ${height}px;
+        `
+      )}
+    >
+      <Map
+        ref={mapRef}
+        center={[options.map.centerLatitude, options.map.centerLongitude]}
+        zoom={options.map.zoom}
+        onload={(event: LeafletEvent) => {
+          onMapLoad(event);
+        }}
+        onmoveend={(event: LeafletEvent) => {
+          onMapMoveEnd(event);
+        }}
+      >
+        {options.viewType === 'ant' && <AntPath positions={antData} options={antOptions} />}
+        {options.viewType === 'heat' && (
+          <HeatmapLayer
+            fitBoundsOnLoad={options.heat.fitBoundsOnLoad}
+            fitBoundsOnUpdate={options.heat.fitBoundsOnUpdate}
+            points={heatData}
+            longitudeExtractor={(m: any) => m[1]}
+            latitudeExtractor={(m: any) => m[0]}
+            intensityExtractor={(m: any) => parseFloat(m[2])}
+          />
+        )}
+        {options.viewType === 'hex' && <WrappedHexbinLayer {...hexbinOptions} data={hexData} />}
+        {options.viewType === 'marker' && markers}
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </Map>
+    </div>
+  );
+};
+
+const getStyles = stylesFactory(() => {
+  return {
+    wrapper: css`
+      position: relative;
+    `,
+  };
+});
---
+++ src/module.ts
@@ -0,0 +1,143 @@
+import { PanelPlugin } from '@grafana/data';
+import { TrackMapOptions } from './types';
+import { TrackMapPanel } from './TrackMapPanel';
+
+export const plugin = new PanelPlugin<TrackMapOptions>(TrackMapPanel).setPanelOptions(builder => {
+  return (
+    builder
+      .addNumberInput({
+        path: 'map.centerLatitude',
+        name: 'Map center latitude',
+        defaultValue: 56.17203,
+      })
+      .addNumberInput({
+        path: 'map.centerLongitude',
+        name: 'Map center longitude',
+        defaultValue: 10.1865203,
+      })
+      .addNumberInput({
+        path: 'map.zoom',
+        name: 'Map Zoom',
+        defaultValue: 10,
+      })
+      .addBooleanSwitch({
+        path: 'map.useBoundsInQuery',
+        name: 'Use map bounds in query',
+        defaultValue: false,
+      })
+      .addRadio({
+        path: 'viewType',
+        defaultValue: 'marker',
+        name: 'Visualisation type',
+        settings: {
+          options: [
+            {
+              value: 'marker',
+              label: 'Markers',
+            },
+            {
+              value: 'ant',
+              label: 'Ant Path',
+            },
+            {
+              value: 'hex',
+              label: 'Hexbin',
+            },
+            {
+              value: 'heat',
+              label: 'Heatmap',
+            },
+          ],
+        },
+      })
+      //ant
+      .addNumberInput({
+        path: 'ant.delay',
+        name: 'Delay',
+        defaultValue: 400,
+        showIf: config => config.viewType === 'ant',
+      })
+      .addNumberInput({
+        path: 'ant.weight',
+        name: 'Weight',
+        defaultValue: 5,
+        showIf: config => config.viewType === 'ant',
+      })
+
+      .addColorPicker({
+        path: 'ant.color',
+        name: 'Color',
+        defaultValue: 'rgb(0, 100, 255)',
+        showIf: config => config.viewType === 'ant',
+      })
+      .addColorPicker({
+        path: 'ant.pulseColor',
+        name: 'Pulse color',
+        defaultValue: 'rgb(0, 0, 0)',
+        showIf: config => config.viewType === 'ant',
+      })
+      .addBooleanSwitch({
+        path: 'ant.paused',
+        name: 'Paused',
+        defaultValue: false,
+        showIf: config => config.viewType === 'ant',
+      })
+      .addBooleanSwitch({
+        path: 'ant.reverse',
+        name: 'Reverse',
+        defaultValue: false,
+        showIf: config => config.viewType === 'ant',
+      })
+      //heat
+      .addBooleanSwitch({
+        path: 'heat.fitBoundsOnLoad',
+        name: 'Fit bounds on load',
+        defaultValue: false,
+        showIf: config => config.viewType === 'heat',
+      })
+      .addBooleanSwitch({
+        path: 'heat.fitBoundsOnUpdate',
+        name: 'Fit bounds on update',
+        defaultValue: false,
+        showIf: config => config.viewType === 'heat',
+      })
+      //marker
+      .addNumberInput({
+        path: 'marker.size',
+        name: 'Size',
+        defaultValue: 25,
+        showIf: config => config.viewType === 'marker',
+      })
+      //hex
+      .addNumberInput({
+        path: 'hex.opacity',
+        name: 'Opacity',
+        defaultValue: 0.6,
+        showIf: config => config.viewType === 'hex',
+      })
+      .addTextInput({
+        path: 'hex.colorRangeFrom',
+        name: 'Color range from (hex)',
+        defaultValue: '#f7fbff',
+        showIf: config => config.viewType === 'hex',
+      })
+      .addTextInput({
+        path: 'hex.colorRangeTo',
+        name: 'Color range to (hex)',
+        defaultValue: '#ff0000',
+        showIf: config => config.viewType === 'hex',
+      })
+      .addNumberInput({
+        path: 'hex.radiusRangeFrom',
+        name: 'Radius range from',
+        defaultValue: 5,
+        showIf: config => config.viewType === 'hex',
+      })
+      .addNumberInput({
+        path: 'hex.radiusRangeTo',
+        name: 'Radius range to',
+        defaultValue: 12,
+        showIf: config => config.viewType === 'hex',
+      })
+  );
+});
